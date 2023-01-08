c4667064cdfeb4429ee8948d038ffd72af49015f
Lon <usmarinec@gmail.com>
2023-01-07 23:54:34 -0700

    Exercise Files

---
+++ xfiles/04/demos/exercise-files/after/NoteKeeper/app/src/main/java/com/jwhh/notekeeper/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.jwhh.notekeeper
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val dm = DataManager()
+        val adapterCourses = ArrayAdapter<CourseInfo>(this,
+                android.R.layout.simple_spinner_item,
+                dm.courses.values.toList())
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCourses.adapter = adapterCourses
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ xfiles/06/demos/exercise-files/before/NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,10 @@
+package com.jwhh.notekeeper
+
+data class CourseInfo (val courseId: String, val title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
+data class NoteInfo(var course: CourseInfo, var title: String, var text: String)
+
