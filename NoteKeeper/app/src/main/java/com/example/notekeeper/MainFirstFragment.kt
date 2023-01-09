01765e5e449ba55dc84c835d092d031cd327c8ab
Lon <usmarinec@gmail.com>
2023-01-09 15:24:46 -0700

    refactored mainActivity layout

--- NoteKeeper/app/src/main/java/com/example/notekeeper/MainFirstFragment.kt
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/MainFirstFragment.kt
@@ -5,15 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.notekeeper.databinding.FragmentFirstBinding
+import com.example.notekeeper.databinding.FragmentMainFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainFirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentMainFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +23,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentMainFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
