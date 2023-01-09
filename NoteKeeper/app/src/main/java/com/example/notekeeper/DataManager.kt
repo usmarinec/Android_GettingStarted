8b1e86240a21b20874813936c4e4b398d2b26a98
Lon <usmarinec@gmail.com>
2023-01-09 11:41:17 -0700

    Fix: switched .set() to indexing operator

--- NoteKeeper/app/src/main/java/com/example/notekeeper/DataManager.kt
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -9,15 +9,15 @@ class DataManager {
     }
     private fun initializeCourses() {
         var course = CourseInfo("android_intents", "Android Programming with Intents")
-        courses.set(course.courseId, course)
+        courses[course.courseId] = course
 
         course = CourseInfo("android_async", "Android Async Programming and Services")
-        courses.set(course.courseId, course)
+        courses[course.courseId] = course
 
         course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
-        courses.set(course.courseId, course)
+        courses[course.courseId] = course
 
         course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
-        courses.set(course.courseId, course)
+        courses[course.courseId] = course
     }
 }
