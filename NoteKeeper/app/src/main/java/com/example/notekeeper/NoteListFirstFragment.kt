2c5603e3f35731e92072fa70fe6655ed50748309
Lon <usmarinec@gmail.com>
2023-01-09 15:38:42 -0700

    Created New Activity NoteListActivity
    Removed second Fragment from new activity && refactored names with imports

---
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/NoteListFirstFragment.kt
@@ -0,0 +1,40 @@
+package com.example.notekeeper
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.notekeeper.databinding.FragmentNoteFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class NoteListFirstFragment : Fragment() {
+
+    private var _binding: FragmentNoteFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNoteFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
